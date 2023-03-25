efd92bc46931cc7740f60bcae6a8d6d76213fb48
Anastacia <nastia.peresypkina2000@gmail.com>
2023-03-25 23:08:55 +0400

    Homework9

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.Write("Введите значение m : ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите значение n: ");
+int n = int.Parse(Console.ReadLine());
+
+int SumElems(int x, int y)
+{
+    if (x > y) return 0;
+    else return x + SumElems(x + 1, y);
+}
+
+Console.WriteLine(SumElems(m,n));
